cc2b7a6eaa7c6c0d24cfa481c818c23a472318b3
Betaser <tylerharrisonhsieh@gmail.com>
2024-01-09 23:09:35 -0500

    Organizing some example game logic into the player class

--- Sources/main.swift
+++ Sources/main.swift
@@ -8,22 +8,48 @@ Raylib.initWindow(screenWidth, screenHeight, "A Bird Game")
 Raylib.setTargetFPS(60)
 let randomColors: [Color] = [.blue, .red, .green, .yellow, .darkBlue, .maroon, .magenta]
 var ballColor: Color = .maroon
-var ballPosition = Vector2(x: -100, y: -100)
 var prevBallPosition: Vector2
+
+class Bird {
+    var pos = Vector2(x: 0, y: 0)
+    var vel = Vector2(x: 0, y: 0)
+    init(pos: Vector2, vel: Vector2) {
+        self.pos = pos
+        self.vel = vel
+    }
+    func updatePhysics() {
+        self.pos = self.pos + self.vel
+    }
+}
+
+var player = Bird(
+    pos: Vector2(x: 100, y: 100),
+    vel: Vector2(x: 0.1, y: 0.1)
+)
+
+// MAIN LOOP
 while Raylib.windowShouldClose == false {
-    prevBallPosition = ballPosition
-    ballPosition = Raylib.getMousePosition()
+    // prevBallPosition = ballPosition
+    prevBallPosition = player.pos
+
+    player.updatePhysics()
+
+    // FOR DEBUGGING REASONS
+    if Raylib.isMouseButtonDown(.left) {
+        player.pos = Raylib.getMousePosition()
+    }
 
     if Raylib.isMouseButtonDown(.left) {
         ballColor = randomColors.randomElement() ?? .black
     }
 
-    let size = max(abs(ballPosition.x - prevBallPosition.x) + abs(ballPosition.y - prevBallPosition.y), 10)
+    let size = max(abs(player.pos.x - prevBallPosition.x) + abs(player.pos.y - prevBallPosition.y), 10)
 
+    // RENDER LOOP
     Raylib.beginDrawing()
     Raylib.clearBackground(.rayWhite)
     Raylib.drawText("Hello, World!", 425, 25, 25, .darkGray)
-    Raylib.drawCircleV(ballPosition, size, ballColor)
+    Raylib.drawCircleV(player.pos, size, ballColor)
     Raylib.drawFPS(10, 10)
     Raylib.endDrawing()
 }
